9a541130c89f9d14d38fc665bcbe7870857f36ee
TaixiL <taixi@email.unc.edu>
2021-11-20 21:14:52 -0500

    s9

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -99,7 +99,12 @@ export const tenTimesFifty = () => {
  *    everyEven([1, 1, 0, 1, 1], x => x === 1)  <--  returns false
  */
 export const everyEven = (arr, test) => {
-
+    for (let i=0; i<arr.length; i++) {
+        if (i%2 == 0 && !test(arr[i])) {
+            return false;
+        }
+    }
+    return true;
 };
 
 
@@ -123,7 +128,12 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
-
+    for (let i=0; i<arr.length; i++) {
+        if (i%2 == 0 && test(arr[i])) {
+            return true;
+        }
+    }
+    return false;
 };
 
 
@@ -149,7 +159,14 @@ export const someEven = (arr, test) => {
  *       -->  { pass: [1, 5, 31], fail: [90] }
  */
 export const filter = (arr, test) => {
-
+    const result = {pass:[], fail:[]};
+    for (let i=0; i<arr.length; i++) {
+        if (test(arr[i])) {
+            result.pass.push(arr[i]);
+        } else {
+            result.fail.push(arr[i]);
+        }
+    }
 };
 
 
@@ -159,7 +176,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    return everyEven(arr, x => x%2 != 0);
 };
 
 
@@ -169,7 +186,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    return someEven(arr, x => x%2 != 0);
 };
 
 
@@ -180,5 +197,6 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-
+    const temp = filter(arr, test);
+    return n==temp.pass.length;
 };
